d5b4f2c1a1d2f61dc30bc5cdcd0ee3c0837ee5ae
Neimerc <Neil_Mercury@yahoo.es>
2021-02-09 19:17:41 +0100

    Inicialización del Proyecto

---
+++ src/router/AppRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Redirect, Router, Route, Switch } from 'react-router-dom';
+
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { LoginScreen } from '../components/auth/LoginScreen';
+
+export const AppRouter = () => {
+  return (
+    <div>
+      <h1>AppRouter</h1>
+    </div>
+  );
+};
