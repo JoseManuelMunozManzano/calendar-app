9abe6da23b5f3f5e792be45ed47b8c5aab07ae36
Neimerc <Neil_Mercury@yahoo.es>
2021-02-09 20:06:47 +0100

    Agregando Rutas

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
 export const AppRouter = () => {
   return (
-    <div>
-      <h1>AppRouter</h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          {/* Usando el component */}
+          <Route exact path="/login" component={LoginScreen} />
+
+          {/* Usando Higher Order Component */}
+          <Route exact path="/">
+            <CalendarScreen />
+          </Route>
+
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 };
