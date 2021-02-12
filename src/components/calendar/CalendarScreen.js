2ce063b90bc65d437972770a07cb9b7671cfd0e1
Neimerc <Neil_Mercury@yahoo.es>
2021-02-12 16:55:30 +0100

    Configuraciones adicionales al calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,8 +3,12 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale('es');
 const localizer = momentLocalizer(moment);
 
 const events = [
@@ -13,10 +17,27 @@ const events = [
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
     bgcolor: '#fafafa',
+    notes: 'Comprar el pastel',
   },
 ];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected);
+
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: '0.8',
+      display: 'block',
+      color: 'white',
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -26,6 +47,8 @@ export const CalendarScreen = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
