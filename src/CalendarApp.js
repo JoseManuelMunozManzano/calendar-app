53fac230812087925a3d56230c8db547cfc60a86
Neimerc <Neil_Mercury@yahoo.es>
2021-02-20 06:35:05 +0100

    Instalación y configuración de Redux

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import { store } from './store/store';
 import { AppRouter } from './router/AppRouter';
 
 export const CalendarApp = () => {
-  return <AppRouter />;
+  // Provider provee la información a todos los hijos
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 };
