f9d3069d51eca0231c0b70ac760d12865be1853b
Neimerc <Neil_Mercury@yahoo.es>
2021-02-25 19:46:50 +0100

    Calendar Backend - Auth Reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,9 +3,10 @@ import { combineReducers } from 'redux';
 
 import { calendarReducer } from './calendarReducer';
 import { uiReducer } from './uiReducer';
+import { authReducer } from './authReducer';
 
 export const rootReducer = combineReducers({
   ui: uiReducer,
   calendar: calendarReducer,
-  // TODO: authReducer
+  auth: authReducer,
 });
