28d4c50e4c6ddf31de50e4639797b768b4be27f2
Neimerc <Neil_Mercury@yahoo.es>
2021-02-25 20:16:53 +0100

    Calendar Backend - Disparar la accion de inicio del login de usuario

--- src/types/types.js
+++ src/types/types.js
@@ -9,10 +9,10 @@ export const types = {
   eventDeleted: '[event] Event deleted',
 
   authChecking: '[auth] Checking login state',
-  authCheckingFinish = '[auth] Finish checking login state',
-  authStartLogin = '[auth] Start login',
-  authLogin = '[auth] Login',
-  authStartRegister = '[auth] Start register',
-  authStartTokenRenew = '[auth] Start token renew',
-  authLogout = '[auth] Logout',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start register',
+  authStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout',
 };
