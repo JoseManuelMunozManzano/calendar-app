d9e749f8c9491e4d7c0b1280c6833a8622982ef4
Neimerc <Neil_Mercury@yahoo.es>
2021-02-28 16:30:55 +0100

    Tests - Pruebas con el Modal y DateTimePicker

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,3 +5,6 @@ import { createSerializer } from 'enzyme-to-json';
 Enzyme.configure({ adapter: new Adapter() });
 
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+// Para evitar errores en test CalendarModal.test.js
+HTMLCanvasElement.prototype.getContext = () => {};
