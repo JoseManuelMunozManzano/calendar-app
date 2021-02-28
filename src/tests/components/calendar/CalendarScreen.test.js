2cb34de4f914d8bd13c4d02608d86725a9776626
Neimerc <Neil_Mercury@yahoo.es>
2021-02-28 16:13:24 +0100

    Tests - Interacciones con el componente Calendar

--- src/tests/components/calendar/CalendarScreen.test.js
+++ src/tests/components/calendar/CalendarScreen.test.js
@@ -4,14 +4,21 @@ import { Provider } from 'react-redux';
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { act } from '@testing-library/react';
 
 import '@testing-library/jest-dom';
 
 import { CalendarScreen } from '../../../components/calendar/CalendarScreen';
+import { messages } from '../../../helpers/calendar-messages-es';
+import { types } from '../../../types/types';
+import { eventSetActive, eventStartLoading } from '../../../actions/events';
 
-// jest.mock('../../../actions/events', () => ({
-//   eventStartDelete: jest.fn(),
-// }));
+jest.mock('../../../actions/events', () => ({
+  eventSetActive: jest.fn(),
+  eventStartLoading: jest.fn(),
+}));
+
+Storage.prototype.setItem = jest.fn();
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -45,5 +52,32 @@ describe('Pruebas en <CalendarScreen />', () => {
 
   test('debe de mostrarse correctamente', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('pruebas con las interacciones del calendario', () => {
+    const calendar = wrapper.find('Calendar');
+
+    // Verificar que calendar tiene los messages
+    const calendarMessages = calendar.prop('messages');
+    expect(calendarMessages).toEqual(messages);
+
+    // Doble click
+    calendar.prop('onDoubleClickEvent')();
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: types.uiOpenModal,
+    });
+
+    // Evento seleccionado. El expect se hace con un mock esta vez
+    calendar.prop('onSelectEvent')({ start: 'Hola' });
+    expect(eventSetActive).toHaveBeenCalledWith({
+      start: 'Hola',
+    });
+
+    // Si cambia el onView debemos asegurarnos de que haya sido cambiado en el localStorage
+    // Se usa act() porque esta instrucción hace una modificación con el setState
+    act(() => {
+      calendar.prop('onView')('week');
+      expect(localStorage.setItem).toHaveBeenCalledWith('lastView', 'week');
+    });
   });
 });
