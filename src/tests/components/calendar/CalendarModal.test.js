fc1f6c9c3edf4471075be001f8922ecb748ca4c7
Neimerc <Neil_Mercury@yahoo.es>
2021-02-28 17:00:51 +0100

    Tests - Debe de llamar la creación de un nuevo evento

--- src/tests/components/calendar/CalendarModal.test.js
+++ src/tests/components/calendar/CalendarModal.test.js
@@ -4,18 +4,23 @@ import { Provider } from 'react-redux';
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { act } from '@testing-library/react';
+import Swal from 'sweetalert2';
 import moment from 'moment';
 
 import '@testing-library/jest-dom';
 import { CalendarModal } from '../../../components/calendar/CalendarModal';
 import {
   eventClearActiveEvent,
+  eventStartAddNew,
   eventStartUpdate,
 } from '../../../actions/events';
 
+Swal.fire = jest.fn();
 jest.mock('../../../actions/events', () => ({
   eventStartUpdate: jest.fn(),
   eventClearActiveEvent: jest.fn(),
+  eventStartAddNew: jest.fn(),
 }));
 
 const middlewares = [thunk];
@@ -75,7 +80,7 @@ describe('Pruebas en <CalendarModal />', () => {
 
     expect(eventClearActiveEvent).toHaveBeenCalled();
 
-    // Aquí se limpió el formulario
+    // Una vez actualizado se limpia el formulario
   });
 
   test('debe de mostrar error si falta el título', () => {
@@ -87,6 +92,80 @@ describe('Pruebas en <CalendarModal />', () => {
 
     expect(wrapper.find('input[name="title"]').hasClass('is-invalid')).toBe(
       true
+    );
+  });
+
+  test('debe de crear un nuevo evento', () => {
+    const initState = {
+      ui: {
+        modalOpen: true,
+      },
+      calendar: {
+        events: [],
+        activeEvent: null,
+      },
+      auth: {
+        uid: '123ABC',
+      },
+    };
+
+    const store = mockStore(initState);
+    store.dispatch = jest.fn();
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <CalendarModal />
+      </Provider>
+    );
+
+    wrapper.find('input[name="title"]').simulate('change', {
+      target: {
+        name: 'title',
+        value: 'Hola pruebas',
+      },
+    });
+
+    wrapper.find('form').simulate('submit', {
+      preventDefault() {},
+    });
+
+    expect(eventStartAddNew).toHaveBeenCalledWith({
+      end: expect.anything(),
+      start: expect.anything(),
+      title: 'Hola pruebas',
+      notes: '',
+    });
+
+    expect(eventClearActiveEvent).toHaveBeenCalled();
+
+    // Una vez se ha insertado se limpia el formulario
+  });
+
+  test('debe de validar las fechas', () => {
+    wrapper.find('input[name="title"]').simulate('change', {
+      target: {
+        name: 'title',
+        value: 'Hola pruebas',
+      },
+    });
+
+    const hoy = new Date();
+
+    // necesario el act() porque actualiza el estado de React.
+    // Tenemos que coger el de @testing-library/react
+    // Nos quedamos con la fecha end, por eso cogemos at(1)
+    act(() => {
+      wrapper.find('DateTimePicker').at(1).prop('onChange')(hoy);
+    });
+
+    wrapper.find('form').simulate('submit', {
+      preventDefault() {},
+    });
+
+    expect(Swal.fire).toHaveBeenCalledWith(
+      'Error',
+      'La fecha fin debe de ser mayor a la fecha de inicio.',
+      'error'
     );
   });
 });
